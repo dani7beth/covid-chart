08734a73ec9d2a64c96e2d08c99bfc94162e7fa0
dani7beth <danielledona28@gmail.com>
2021-01-04 10:28:02 -0700

    added data fallback

--- src/CovidChart.js
+++ src/CovidChart.js
@@ -1,31 +1,37 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { USData } from "./hardCodedData";
 
 const options = {
   method: "GET",
   url: "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats",
-  params: { country: "Canada" },
+  params: { country: "US" },
   headers: {
-    "x-rapidapi-key": "7c624aa7eamsh43c79ef705599ccp13dd06jsn579dd136078f",
+    "x-rapidapi-key": process.env.REACT_APP_API_KEY,
     "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
   },
 };
 
 const CovidChart = () => {
+
+  const [chartData, setChartData] = useState([]);
   useEffect(() => {
     axios
       .request(options)
       .then(function (response) {
         console.log(response.data.data.covid19Stats);
+        setChartData(response.data.data.covid19Stats);
       })
       .catch(function (error) {
         console.error(error);
+        setChartData(USData);
       });
   }, []);
 
   return (
     <div>
       <h1>Stats</h1>
+      <p>{chartData.length}</p>
     </div>
   );
 };
